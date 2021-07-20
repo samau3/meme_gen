008343202f31f0243047c1f44cdc6399c5abb0d1
samau3 <samau415@gmail.com>
2021-07-20 13:40:10 -0700

    basic functionality and skeleton working

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const form = document.querySelector('form')
+const memeList = document.querySelector('#memes')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    let imageInput = form.elements.imageLink.value
+    let topInput = form.elements.topText.value
+    let bottomInput = form.elements.bottomText.value
+    addMeme(imageInput, topInput, bottomInput)
+    imageInput = ''
+    topInput = ''
+    bottomInput = ''
+})
+
+const addMeme = (image, top, bottom) => {
+    const newLi = document.createElement('li');
+    newLi.innerText = (`${image}, ${top}, ${bottom}`)
+    memeList.appendChild(newLi)
+}
