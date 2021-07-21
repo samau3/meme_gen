143152f44571c2c189a0d8869f3df6679fcc3c29
samau3 <samau415@gmail.com>
2021-07-21 16:00:58 -0700

    improved styling and layout of elements

--- app.js
+++ app.js
@@ -22,24 +22,29 @@ const addMeme = (image, top, bottom) => {
     let newImage = document.createElement('img')
     let topText = document.createElement('p')
     let bottomText = document.createElement('p')
-
+    let deleteText = document.createElement('p')
     let topDiv = document.createElement('div')
     let bottomDiv = document.createElement('div')
+    let deleteDiv = document.createElement('div')
 
     topDiv.appendChild(topText)
     bottomDiv.appendChild(bottomText)
+    deleteDiv.appendChild(deleteText)
 
     topDiv.classList.add('topText')
     bottomDiv.classList.add('bottomText')
-
+    deleteDiv.classList.add('deleteInfo')
 
     newImage.src = image
     topText.innerText = top
     bottomText.innerText = bottom
+    deleteText.innerText = "X"
+
 
     newMeme.appendChild(newImage)
     newMeme.appendChild(topDiv)
     newMeme.appendChild(bottomDiv)
+    newMeme.appendChild(deleteDiv)
     memeSection.appendChild(newMeme)
 
     newMeme.addEventListener("click", function (e) {
